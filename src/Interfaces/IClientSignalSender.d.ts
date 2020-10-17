b7eb5c4e65405ecedaff6176ad9780b37d44e9ca
Noah Crowley <noahwillcrow@gmail.com>
2020-10-16 23:46:34 -0400

    Add export

--- src/Interfaces/IClientSignalSender.d.ts
+++ src/Interfaces/IClientSignalSender.d.ts
@@ -4,7 +4,7 @@ import { NetworkedSignalCallback } from "../types/NetworkedSignalCallback";
 /**
  * Defines the interface for sending remote signals from the client to the server
  */
-interface IClientSignalSender<T extends NetworkedSignalCallback = () => void> extends IDestroyable {
+export interface IClientSignalSender<T extends NetworkedSignalCallback = () => void> extends IDestroyable {
 	/**
 	 * Fires a signal from the client to the server
 	 * @param args The arguments to send with the signal
